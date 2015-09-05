7454f217544a43e12612bb6ebaa4b5931fc2069f
Grant Miner <xxgsoftware@gmail.com>
2015-09-05 11:53:17 -0500

    Reindent and change file names

---
+++ brute-rethinkdb.js
@@ -0,0 +1,89 @@
+'use strict';
+var AbstractClientStore = require('express-brute/lib/AbstractClientStore');
+
+var RethinkdbStore = module.exports = function (options) {
+    var self = this;
+    AbstractClientStore.apply(this, arguments);
+    self.options = options || {};
+    if (!self.options.table) {
+        self.options.table = 'brute'
+    }
+
+    if (typeof self.options === 'function') {
+        self.r = self.options;
+    } else if (typeof self.options === 'object') {
+        self.r = require('rethinkdbdash')(self.options);
+    } else if (options === null || options === undefined) {
+        self.r = require('rethinkdbdash')();
+        options = {};
+    } else {
+        throw new TypeError('Invalid options');
+    }
+
+    self.r.tableCreate(self.options.table)
+    .run()
+    .catch(function (error) {
+        if (!error.message.indexOf('already exists') > 0) {
+            throw error;
+        }
+    })
+    .then(function () {
+        return self.r
+        .table(self.options.table)
+        .indexCreate('lifetime')
+        .run()
+        .catch(function (error) {
+            if (!error.message.indexOf('already exists') > 0) {
+                throw error;
+            }
+        })
+    })
+
+    self.clearInterval = setInterval(self.clearExpired.bind(self),
+    self.options.clearInterval || 60000).unref();
+};
+
+RethinkdbStore.prototype = Object.create(AbstractClientStore.prototype);
+RethinkdbStore.prototype.set = function (key, value, lifetime, callback) {
+    var self = this;
+    lifetime = lifetime || 0;
+
+    return self.r.table(self.options.table)
+    .insert({
+        id: key,
+        lifetime: new Date(Date.now() + lifetime  * 1000),
+        lastRequest: value.lastRequest,
+        firstRequest: value.firstRequest,
+        count: value.count
+    }, {
+        conflict: 'update'
+    })
+    .run()
+    .asCallback(callback);
+};
+
+RethinkdbStore.prototype.get = function (key, callback) {
+    var self = this;
+    return self.r.table(self.options.table)
+    .get(key)
+    .run()
+    .asCallback(callback);
+};
+
+RethinkdbStore.prototype.reset = function (key, callback) {
+    var self = this;
+    return self.r.table(self.options.table)
+    .get(key)
+    .delete()
+    .run()
+    .asCallback(callback);
+};
+
+RethinkdbStore.prototype.clearExpired = function (callback) {
+    var self = this;
+    return self.r.table(self.options.table)
+    .filter(self.r.row('lifetime').lt(new Date()))
+    .delete()
+    .run()
+    .asCallback(callback);
+};
--- test.js
+++ test.js
@@ -1,112 +1,111 @@
 'use strict';
 const Promise = require('bluebird');
 const test = require('tape');
-const Brute = require('./index.js');
+const Brute = require('./brute-rethinkdb.js');
 const store = Promise.promisifyAll(new Brute());
 
 const name ='';
 
 test(name +' properly instantiate', function (t) {
-  t.plan(2);
+    t.plan(2);
 
-  t.ok(store, name +' exists');
-  t.ok(store instanceof Brute, name +' instanceof');
+    t.ok(store, name +' exists');
+    t.ok(store instanceof Brute, name +' instanceof');
 });
 
 
 test(name +' return null when no value is available', function (t) {
-  t.plan(1);
+    t.plan(1);
 
-  store.get('novalue').then(function (result) {
-    t.equal(result, null, name +' novalue null');
-  });
+    store.get('novalue').then(function (result) {
+        t.equal(result, null, name +' novalue null');
+    });
 });
 
 test(name +' set records and get them back', function (t) {
-  t.plan(1);
-  const curDate = new Date();
-  const object = {count: 17, lastRequest: curDate, firstRequest: curDate};
+    t.plan(1);
+    const curDate = new Date();
+    const object = {count: 17, lastRequest: curDate, firstRequest: curDate};
 
-  store.set('set records', object, 10*1000)
-  .then(function () {
-    return store.get('set records').then(function (result) {
-      t.equal(result.count, 17, name +' result should be 17');
+    store.set('set records', object, 10*1000)
+    .then(function () {
+        return store.get('set records').then(function (result) {
+            t.equal(result.count, 17, name +' result should be 17');
+        });
     });
-  });
 });
 
 test(name + ' set records, not get them back if they expire', function (t) {
-  t.plan(2);
-  const curDate = new Date();
-  const object = {count: 17, lastRequest: curDate, firstRequest: curDate};
+    t.plan(2);
+    const curDate = new Date();
+    const object = {count: 17, lastRequest: curDate, firstRequest: curDate};
 
-  store.set('1234expire', object, 0)
-  .then(function () {
-    return store.clearExpired();
-  })
-  .then(function (result) {
-    return t.notEqual(result[0], null); // 1 row should be updated ??
-  })
-  .then(function () {
-    return store.get('1234expire').then(function (result) {
-      t.equal(result, null, name + ' null result');
+    store.set('1234expire', object, 0)
+    .then(function () {
+        return store.clearExpired();
+    })
+    .then(function (result) {
+        return t.notEqual(result[0], null); // 1 row should be updated ??
+    })
+    .then(function () {
+        return store.get('1234expire').then(function (result) {
+            t.equal(result, null, name + ' null result');
+        });
     });
-  });
 });
 
 test(name + ' reset (delete) a record', function (t) {
-  t.plan(1);
+    t.plan(1);
 
-  const curDate = new Date();
-  const object = {count: 36713, lastRequest: curDate, firstRequest: curDate};
-  const key = "reset1.2.3.4";
+    const curDate = new Date();
+    const object = {count: 36713, lastRequest: curDate, firstRequest: curDate};
+    const key = "reset1.2.3.4";
 
-  store.set(key, object, 10 * 1000)
-  .then(function () {
-    return store.reset(key);
-  })
-  .then(function (res) {
-    return store.get(key);
-  })
-  .then(function (res) {
-    t.equal(res, null, name +' is null')
-  })
+    store.set(key, object, 10 * 1000)
+    .then(function () {
+        return store.reset(key);
+    })
+    .then(function (res) {
+        return store.get(key);
+    })
+    .then(function (res) {
+        t.equal(res, null, name +' is null')
+    })
 });
 
 test(name + ' increment even if not originally set', function (t) {
-  t.plan(1);
+    t.plan(1);
 
-  const key = "incrementtest";
+    const key = "incrementtest";
 
-  store.incrementAsync(key, 10 * 1000)
-  .then(function () {
-    return store.get(key)
-    .then(function (result) {
-      t.equal(result.count, 1, 'count should be 1')
+    store.incrementAsync(key, 10 * 1000)
+    .then(function () {
+        return store.get(key)
+        .then(function (result) {
+            t.equal(result.count, 1, 'count should be 1')
+        })
     })
-  })
 });
 
 test(name + ' expires', function (t) {
-  t.plan(1);
+    t.plan(1);
 
-  const curDate = new Date();
-  const object = {count: 1, lastRequest: curDate, firstRequest: curDate};
+    const curDate = new Date();
+    const object = {count: 1, lastRequest: curDate, firstRequest: curDate};
 
-  store.incrementAsync('expiring', 0)
-  .then(function () {
-    return store.clearExpired();
-  })
-  .then(function (result) {
-    return store.get('expiring');
-  })
-  .then(function (result) {
-    t.equal(result, null);
-  })
+    store.incrementAsync('expiring', 0)
+    .then(function () {
+        return store.clearExpired();
+    })
+    .then(function (result) {
+        return store.get('expiring');
+    })
+    .then(function (result) {
+        t.equal(result, null);
+    })
 });
 
 test('process exit', function (t) {
-  t.end();
-  process.exit();
+    t.end();
+    process.exit();
 })
-
